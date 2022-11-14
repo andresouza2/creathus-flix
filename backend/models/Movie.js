13c37cfc92b513df3d7ac9b3622ead882249828f
Andre <andssouzza@gmail.com>
2022-11-14 09:41:52 -0400

    fix: melhoria no código do arquivo Movie.js no repositório ../models/..

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -1,27 +1,13 @@
 const mongoose = require('../db/conn');
 const { Schema } = mongoose;
 
-const Movie = mongoose.model(
-  'Movie',
-  new Schema({
-    author: {
-      type: String,
-      required: true
-    },
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true
-    },
-    image: {
-      type: String
-    }
-  },
-    { timestamps: true },
-  ),
-)
+const movieSchema = new Schema({
+  author: { type: String, required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: false }
+}, { timestamps: true })
+
+const Movie = mongoose.model('movie', movieSchema)
 
 module.exports = Movie
