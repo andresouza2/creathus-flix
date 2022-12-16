5b07967cd25e33491c56fb912fe915765e68ce34
Andre <andssouzza@gmail.com>
2022-12-15 22:26:46 -0400

    refatorando o meu código

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,7 +1,5 @@
-// import { Outlet } from 'react-router-dom'
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../../layouts/Sidebar'
-// import MoviesAll from '../Movies/MoviesAll'
 import { Component } from './style'
 
 const Home: React.FC = () => {
